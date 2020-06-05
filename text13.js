d5dd886c12048e19e9b0add46fb32183cc5ec71a
srishtij2000 <64734684+srishtij2000@users.noreply.github.com>
2020-06-05 18:58:17 +0530

    drum kit

---
+++ text13.js
@@ -0,0 +1,16 @@
+function playsound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //to check that no other letter is there
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playsound);
